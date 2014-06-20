a5a2ff10f15c8e55c71b1a0bbe70251716f9afad
Aron Szekely <aron@indispeq.com>
2014-06-20 16:24:31 -0400

    user and vmwaretools works

--- production/modules/user/manifests/sudoers.pp
+++ production/modules/user/manifests/sudoers.pp
@@ -1,9 +1,18 @@
 class user::sudoers {
 
-	file { '/etc/sudoers':
-		owner => root,
-		group => root,
-		mode => 0440,
-		source => 'puppet:///modules/user/sudoers',
+	package { 'sudo':
+		ensure => installed,
 	}
+
+	augeas {'passwordless sudo':
+		context => '/files/etc/sudoers',
+		changes => [
+			'set spec[user = "%sudo"]/user %sudo',
+			'set spec[user = "%sudo"]/host_group/host ALL',
+			'set spec[user = "%sudo"]/host_group/command ALL',
+			'set spec[user = "%sudo"]/host_group/command/tag NOPASSWD',
+			'set spec[user = "%sudo"]/host_group/command/runas_user ALL',
+		]
+	}
+
 }
